6bb3d797d4d53725876b6e8df77161da841338c4
irohitb <irohitbhatia@gmail.com>
2019-04-16 04:47:35 +0530

    added code snipprt to see if service worker and push Manager is supported

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('js is working')
+const check = () => {
+    if (!('serviceWorker' in navigator)) {
+      throw new Error('No Service Worker support!')
+    }
+    if (!('PushManager' in window)) {
+      throw new Error('No Push API Support!')
+    }
+    console.log(`Browser supports Pushmanager and Service worker`)
+  }
+  const main = () => {
+    check()
+  }
+  main()
